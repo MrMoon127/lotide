7005af6c565a8b13b85008f8c0d02f671f03a643
Sasha Andia <sasha.andia@gmail.com>
2023-05-20 10:27:05 -0700

    contains a function that determines if two arrays are equal. Does not work on nested arrays

---
+++ eqArrays.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔⛔⛔Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const eqArrays = function(arr1, arr2) {
+  // if the two arrays are not the same length they cannot be equal
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  // we don't need another loop to check arr2 because they should be the same in each position
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  // we've determined all false senarios, so it must be true
+  return true;
+
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
