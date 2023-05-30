f5db979a2e50de4b1d49017c38dfdae40d7fda7d
Sasha Andia <sasha.andia@gmail.com>
2023-05-29 17:56:33 -0700

    function that returns an object of letters from a given sentence and how many of each letter there is

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔⛔⛔Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  let results = {};
+
+  for (const letter of sentence) {
+    if (letter !== " ") {
+      if (results[letter]) {
+        results[letter]++;
+      } else {
+        results[letter] = 1;
+      }
+    }
+  }
+
+  return results;
+}
+
+
+console.log(countLetters("lighthouse in the hosue"));
