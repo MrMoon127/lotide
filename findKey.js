837dec65c8c0380ec068ee35d94b16728943e4f7
Sasha Andia <sasha.andia@gmail.com>
2023-06-01 14:47:11 -0700

    takes an object and a callback function and returns the first key that returns a truthy value, returns undefined otherwise

---
+++ findKey.js
@@ -0,0 +1,43 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔⛔⛔Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// FUNCTION IMPLEMENTATION
+const findKey = function(object, callback) {
+  let toReturn;
+
+  for (const key in object) {
+    if (callback(object[key])) {
+      toReturn = key;
+      return toReturn;
+    }
+  }
+
+  return toReturn;
+}
+
+
+// TEST CASES
+const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+assertEqual(result1, "noma");
+
+const result2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 4)
+assertEqual(result2, undefined);
