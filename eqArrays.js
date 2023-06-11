a47c3f15fec49221b893dc3ef9ce3f5c109af488
Sasha Andia <sasha.andia@gmail.com>
2023-06-10 22:21:53 -0700

    added a test file and took testing out of the function

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,4 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`⛔⛔⛔Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(arr1, arr2) {
   // if the two arrays are not the same length they cannot be equal
@@ -26,8 +18,4 @@ const eqArrays = function(arr1, arr2) {
 
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays;
