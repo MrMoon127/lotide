f4b065fac241ee94571284a2128120bdf2ba1434
Sasha Andia <sasha.andia@gmail.com>
2023-06-10 22:33:28 -0700

    refactored so that tests were in a separate file

--- middle.js
+++ middle.js
@@ -1,31 +1,3 @@
-// TEST/ASSERTION FUNCTIONS
-
-const eqArrays = function(arr1, arr2) {
-  // if the two arrays are not the same length they cannot be equal
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  // we don't need another loop to check arr2 because they should be the same in each position
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  // we've determined all false senarios, so it must be true
-  return true;
-
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅Arrays are equal: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`⛔⛔⛔Arrays are not equal: ${arr1} !== ${arr2}`);
-  }
-};
-
 // ACTUAL FUNCTION
 const middle = function(arr) {
   let returnArr = []
@@ -48,12 +20,4 @@ const middle = function(arr) {
 }
 
 
-// TEST CODE
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle
