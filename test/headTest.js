9f5637d6f6fe1088afe14ed2ff2637cbc08b4e57
Sasha Andia <sasha.andia@gmail.com>
2023-06-10 22:50:16 -0700

    changed so that some files would use mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,14 @@
 
 const head = require('../head')
-const assertEqual = require('../assertEqual')
+const assert = require('chai').assert;
 
-assertEqual(3, head([3, 2, 1]));
-assertEqual(5, head(3, 4, 7));
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([8]), 8);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+});
