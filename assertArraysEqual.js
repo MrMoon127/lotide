1f4c71783593fb0ad4209f5d8cfa042a4a32a942
Sasha Andia <sasha.andia@gmail.com>
2023-06-10 22:27:15 -0700

    refactored so that it will call on functions from other files

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  // if the two arrays are not the same length they cannot be equal
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  // we don't need another loop to check arr2 because they should be the same in each position
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  // we've determined all false senarios, so it must be true
-  return true;
-
-};
+const eqArrays = require('./eqArrays')
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
@@ -24,7 +8,4 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([], []);
-assertArraysEqual([1, 2], [1, 2, 3]);
-assertArraysEqual([1, 3, 2], [1, 2, 3]);
+module.exports = assertArraysEqual;
